95ace2b9ce7334f7a4646951bbfef41ffce96817
snowdream <sn0wdr1am@qq.com>
2024-06-20 15:22:02 +0800

    feat: github-actions with self-hosted

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ DOCKER_HUB_PROJECT=snowdreamtech/alpine
 GITHUB_PROJECT=ghcr.io/snowdreamtech/alpine
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
--t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:3.20.0 \
--t ${DOCKER_HUB_PROJECT}:3.20 \
--t ${DOCKER_HUB_PROJECT}:3 \
--t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:3.20.0 \
--t ${GITHUB_PROJECT}:3.20 \
--t ${GITHUB_PROJECT}:3 \
+-t ${DOCKER_HUB_PROJECT}:entrypoint-latest \
+-t ${DOCKER_HUB_PROJECT}:entrypoint-3.20.0 \
+-t ${DOCKER_HUB_PROJECT}:entrypoint-3.20 \
+-t ${DOCKER_HUB_PROJECT}:entrypoint-3 \
+-t ${GITHUB_PROJECT}:entrypoint-latest \
+-t ${GITHUB_PROJECT}:entrypoint-3.20.0 \
+-t ${GITHUB_PROJECT}:entrypoint-3.20 \
+-t ${GITHUB_PROJECT}:entrypoint-3 \
 . \
 --push
