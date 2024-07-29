2334b56efcea2d71f68fa7006e9873db67966e55
snowdream <sn0wdr1am@qq.com>
2024-07-29 11:07:08 +0800

    feat： alpine:3.20.2

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ GITHUB_PROJECT=ghcr.io/snowdreamtech/alpine
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
 -t ${DOCKER_HUB_PROJECT}:entrypoint-latest \
--t ${DOCKER_HUB_PROJECT}:entrypoint-3.20.1 \
+-t ${DOCKER_HUB_PROJECT}:entrypoint-3.20.2 \
 -t ${DOCKER_HUB_PROJECT}:entrypoint-3.20 \
 -t ${DOCKER_HUB_PROJECT}:entrypoint-3 \
 -t ${GITHUB_PROJECT}:entrypoint-latest \
--t ${GITHUB_PROJECT}:entrypoint-3.20.1 \
+-t ${GITHUB_PROJECT}:entrypoint-3.20.2 \
 -t ${GITHUB_PROJECT}:entrypoint-3.20 \
 -t ${GITHUB_PROJECT}:entrypoint-3 \
 . \
