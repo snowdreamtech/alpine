152c91a3b3d477947ef92cbd608d9544c38a1a39
snowdream <sn0wdr1am@qq.com>
2024-09-14 22:17:14 +0800

    feat: upgrade alpine:3.20.3

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ GITHUB_PROJECT=ghcr.io/snowdreamtech/alpine
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
 -t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:3.20.2 \
+-t ${DOCKER_HUB_PROJECT}:3.20.3 \
 -t ${DOCKER_HUB_PROJECT}:3.20 \
 -t ${DOCKER_HUB_PROJECT}:3 \
 -t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:3.20.2 \
+-t ${GITHUB_PROJECT}:3.20.3 \
 -t ${GITHUB_PROJECT}:3.20 \
 -t ${GITHUB_PROJECT}:3 \
 . \
