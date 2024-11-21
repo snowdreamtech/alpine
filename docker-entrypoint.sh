dac6f30dd2883dc4b57cef7106da06c2c335edf6
snowdream <sn0wdr1am@qq.com>
2024-11-21 17:46:21 +0800

    fix: warning for variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -27,10 +27,10 @@ echo "root:$SSH_ROOT_PASSWORD" | chpasswd >/dev/null 2>&1
 
 # generate ssh keys
 if [ ! -d "/root/.ssh" ]; then
-  ssh-keygen -t ed25519 -C "user@example.com" -f $HOME/.ssh/id_ed25519 -q -N ""
-  ssh-keygen -t rsa -b 4096 -C "user@example.com" -f $HOME/.ssh/id_rsa -q -N ""
-  ssh-keygen -t ecdsa -b 521 -C "user@example.com" -f $HOME/.ssh/id_ecdsa -q -N ""
-  ssh-keygen -t dsa -C "user@example.com" -f $HOME/.ssh/id_dsa -q -N ""
+  ssh-keygen -t ed25519 -C "user@example.com" -f "$HOME"/.ssh/id_ed25519 -q -N ""
+  ssh-keygen -t rsa -b 4096 -C "user@example.com" -f "$HOME"/.ssh/id_rsa -q -N ""
+  ssh-keygen -t ecdsa -b 521 -C "user@example.com" -f "$HOME"/.ssh/id_ecdsa -q -N ""
+  ssh-keygen -t dsa -C "user@example.com" -f "$HOME"/.ssh/id_dsa -q -N ""
 fi
 
 # start sshd
