a97e8e51fbdcec685c8214b65fdfe36c45a2db40
snowdream <sn0wdr1am@qq.com>
2024-12-10 15:42:42 +0800

    feat: x11vnc with xvfb

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 # setup-keymap
 setup-keymap.exp >/dev/null 2>&1
 
-
 # ssh-keygen -A
 if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
   ssh-keygen -A >/dev/null 2>&1
@@ -19,7 +18,7 @@ crond >/dev/null 2>&1
 # openssl rand -base64 33
 if [ -z "${SSH_ROOT_PASSWORD}" ]; then {
     SSH_ROOT_PASSWORD=$(openssl rand -base64 33)
-    echo "Generate random ssh root password:${SSH_ROOT_PASSWORD}"
+    echo "Generate random ssh root password: ${SSH_ROOT_PASSWORD}"
 }
 fi
 
@@ -35,7 +34,25 @@ if [ ! -d "/root/.ssh" ]; then
 fi
 
 # start sshd
-/usr/sbin/sshd -D
+/usr/sbin/sshd -D > /dev/null 2>&1 &
+
+# start Xvfb
+# nohup /usr/bin/Xvfb "$DISPLAY" -screen 0 "$RESOLUTION" -ac +extension GLX +render -noreset > /dev/null 2>&1 &
+/usr/bin/Xvfb "$DISPLAY" -screen 0 "$RESOLUTION" -ac +extension GLX +render -noreset &
+
+# start x11vnc
+
+# openssl rand -base64 33
+if [ -z "${VNC_ROOT_PASSWORD}" ]; then {
+    VNC_ROOT_PASSWORD=$(openssl rand -base64 33)
+    echo "Generate random vnc root password: ${VNC_ROOT_PASSWORD}"
+}
+fi
+
+x11vnc -storepasswd "$VNC_ROOT_PASSWORD" /etc/x11vnc.pass > /dev/null 2>&1 &
+
+# nohup x11vnc -listen 0.0.0.0  -auth guess -unixpw --rfbport "$VNC_PORT" -display "$DISPLAY" -bg -wait 20 -loop -forever -shared > /dev/null 2>&1 &
+x11vnc  -listen 0.0.0.0 -rfbauth /etc/x11vnc.pass -rfbport "$VNC_PORT" -display "$DISPLAY" -wait 20 -loop -forever -shared > /dev/null 2>&1 &
 
 # exec commands
 if [ -n "$*" ]; then
