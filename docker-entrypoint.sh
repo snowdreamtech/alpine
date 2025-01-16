cb4e5ddeb7514b930eb20b603950422d5adb5070
snowdream <sn0wdr1am@qq.com>
2025-01-16 19:34:07 +0800

    feat： openssh with x11 forwarding

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,38 @@
 #!/bin/sh
 set -e
 
+# ssh-keygen -A
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+  ssh-keygen -A >/dev/null 2>&1
+fi
+
+# start cornd
+crond >/dev/null 2>&1
+
+# generate motd
+/etc/periodic/15min/motd.sh >/dev/null 2>&1
+
+# openssl rand -base64 33
+if [ -z "${SSH_ROOT_PASSWORD}" ]; then {
+    SSH_ROOT_PASSWORD=$(openssl rand -base64 33)
+    echo "Generate random ssh root password:${SSH_ROOT_PASSWORD}"
+}
+fi
+
+# change the password for root
+echo "root:$SSH_ROOT_PASSWORD" | chpasswd >/dev/null 2>&1
+
+# generate ssh keys
+if [ ! -d "/root/.ssh" ]; then
+  ssh-keygen -t ed25519 -C "user@example.com" -f $HOME/.ssh/id_ed25519 -q -N ""
+  ssh-keygen -t rsa -b 4096 -C "user@example.com" -f $HOME/.ssh/id_rsa -q -N ""
+  ssh-keygen -t ecdsa -b 521 -C "user@example.com" -f $HOME/.ssh/id_ecdsa -q -N ""
+  ssh-keygen -t dsa -C "user@example.com" -f $HOME/.ssh/id_dsa -q -N ""
+fi
+
+# start sshd
+/usr/sbin/sshd -D
+
 # exec commands
 if [ -n "$*" ]; then
   sh -c "$*"
---
+++ motd.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cat > /etc/motd <<EOF
+Welcome to Alpine!
+
+The Alpine Wiki contains a large amount of how-to guides and general
+information about administrating Alpine systems.
+See <https://wiki.alpinelinux.org/>.
+
+You can setup the system with the command: setup-alpine
+
+You may change this message by editing /etc/motd.
+
+$(fastfetch --pipe false)
+
+
+
+EOF
+
