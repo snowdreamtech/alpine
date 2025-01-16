68b68bc5fafad1e920d270b5ae5b91720c871776
snowdream <sn0wdr1am@qq.com>
2025-01-16 19:32:59 +0800

    format: docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,13 +3,14 @@ set -e
 
 # exec commands
 if [ -n "$*" ]; then
-    sh -c "$*"
+  sh -c "$*"
 fi
 
 # keep the docker container running
 # https://github.com/docker/compose/issues/1926#issuecomment-422351028
 if [ "${KEEPALIVE}" -eq 1 ]; then
-    trap : TERM INT
-    tail -f /dev/null & wait
-    # sleep infinity & wait
+  trap : TERM INT
+  tail -f /dev/null &
+  wait
+  # sleep infinity & wait
 fi
