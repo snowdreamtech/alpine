76b5f5c72f22db212e72c8fcdcea189e53fad132
snowdream <sn0wdr1am@qq.com>
2026-03-03 08:36:37 +0800

    chore(commitlint): increase max commit message length to 120 for AI compatibility

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "header-max-length": [2, "always", 100],
-    "subject-max-length": [2, "always", 100],
-    "body-max-line-length": [2, "always", 100],
-    "footer-max-line-length": [2, "always", 100],
+    "header-max-length": [2, "always", 120],
+    "subject-max-length": [2, "always", 120],
+    "body-max-line-length": [2, "always", 120],
+    "footer-max-line-length": [2, "always", 120],
     "type-enum": [
       2,
       "always",
