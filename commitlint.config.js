b76679147dc7543a612da1d2f4637154e8857776
snowdream <sn0wdr1am@qq.com>
2026-03-03 08:25:47 +0800

    chore(commitlint): translate custom rule comment to English

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "header-max-length": [2, "always", 100],
+    "header-max-length": [2, "always", 120],
+    "subject-max-length": [2, "always", 120],
+    "body-max-line-length": [2, "always", 120],
+    "footer-max-line-length": [2, "always", 120],
     "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore", "ci", "perf", "build"]],
+
+    // Custom rule: Disallow Chinese characters in commit messages
+    "no-chinese": [2, "always"],
   },
+  plugins: [
+    {
+      rules: {
+        "no-chinese": ({ header, body, footer }) => {
+          const text = `${header}\n${body}\n${footer}`;
+          const hasChinese = /[\u4e00-\u9fa5]/.test(text);
+          return [!hasChinese, "Commit message must be in English only (no Chinese characters allowed)."];
+        },
+      },
+    },
+  ],
 };
