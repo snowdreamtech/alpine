4c7b808e96a0dd2cebdabd3ff9607e881afc3249
snowdream <sn0wdr1am@qq.com>
2026-03-03 08:23:44 +0800

    build(commitlint): explicit configuration mapping to coding style

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore", "ci", "perf", "build"]],
+  },
 };
